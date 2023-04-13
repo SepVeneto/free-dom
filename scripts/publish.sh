edbc1d8a6f403a9b6ae981d928569bcb03cf53fa
SepVeneto <sepveneto@qq.com>
2023-04-13 20:32:27 +0800

    ci: update  publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,6 +2,6 @@
 
 pnpm i --frozen-lockfile --registry=https://registry.npmmirror.com
 
-cd packages/core && pnpm build && pnpm publish --access public
+cd packages/core && pnpm build && pnpm publish --no-git-checks --access public
 
 curl -X PUT https://registry-direct.npmmirror.com/@sepveneto/free-dom/sync?sync_upstream=true
