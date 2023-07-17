94bede0a58b5e454d9800a643ca34ed9d290167e
SepVeneto <sepveneto@qq.com>
2023-07-17 17:04:43 +0800

    fix(free-dom): 使用额外字体时导致的元素尺寸计算不准确

--- packages/core/src/hooks/use-resizable-data.ts
+++ packages/core/src/hooks/use-resizable-data.ts
@@ -1,6 +1,6 @@
 import type { FreeDomProps } from '../components/freeDom'
 import type FreeDomCore from '../components/freeDomCore'
-import { onMounted, ref, watchEffect } from 'vue-demi'
+import { nextTick, onMounted, ref, watchEffect } from 'vue-demi'
 import type { Ref } from 'vue-demi'
 
 export function useResizableData(
@@ -25,12 +25,17 @@ export function useResizableData(
   function getHeight() {
     return props.height || props.modelValue.h
   }
-  function syncSize() {
+  async function syncSize() {
     if ((props.width && props.height) || (props.modelValue.w && props.modelValue.h)) return
     if (!domRef.value) return [0, 0]
-    const { width: w, height: h } = domRef.value.$el.getBoundingClientRect()
-    width.value = Math.max(Math.ceil(w), props.minWidth)
-    height.value = Math.max(Math.ceil(h), props.minHeight)
+
+    // 确保css样式表解析完成重新渲染，同时字体文件加载完成
+    await nextTick()
+    await document.fonts.ready
+
+    const { width: w, height: h } = window.getComputedStyle(domRef.value.$el)
+    width.value = Math.max(Math.ceil(parseFloat(w)), props.minWidth)
+    height.value = Math.max(Math.ceil(parseFloat(h)), props.minHeight)
   }
 
   return {
