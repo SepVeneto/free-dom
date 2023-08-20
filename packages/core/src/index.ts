81e0d7719a6e629ef7e56ac8f0bce1e2904ddc0f
SepVeneto <sepveneto@qq.com>
2023-08-20 14:01:17 +0800

    fix: no export core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,8 +2,13 @@ import './style/index.scss'
 // import { FreeDom } from './components/freeDom'
 import { FreeDomWrap } from './components/freeDomWrap'
 import _GridLayout from './components/gridLayout'
-import _FreeDomCore from './components/freeDom'
+import _FreeDom from './components/freeDom'
+import _FreeDomCore from './components/freeDomCore'
+import _ResizeDomCore from './components/resizeDomCore'
+
 // export const freeDom = FreeDom
 export const FreeScene = FreeDomWrap
 export const GridLayout = _GridLayout
-export const FreeDom = _FreeDomCore
+export const FreeDom = _FreeDom
+export const FreeDomCore = _FreeDomCore
+export const ResizeDomCore = _ResizeDomCore
