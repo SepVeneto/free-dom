8be98f5f5c7ff8f805a0bb15f112ae763e86e4f1
SepVeneto <sepveneto@qq.com>
2024-01-25 14:12:42 +0800

    feat(free-dom): shift ignore lock aspect ratio

--- packages/core/src/components/resizeDomCore.ts
+++ packages/core/src/components/resizeDomCore.ts
@@ -112,9 +112,10 @@ const resizeDomCore = defineComponent({
 
         let width = props.width + (canDragX ? deltaX : 0)
         let height = props.height + (canDragY ? deltaY : 0)
-
         // 这里不加分号会导致语法错误
-        ;[width, height] = runConstraints(width, height)
+        if (!evt.shiftKey) {
+          [width, height] = runConstraints(width, height)
+        }
 
         const sizeChanged = width !== props.width || height !== props.height
 
