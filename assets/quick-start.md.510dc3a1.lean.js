4ca65d2da60724e242522b4af152209cd7418d3b
SepVeneto <SepVeneto@users.noreply.github.com>
2024-12-12 07:24:30 +0000

    Deploying to docs from @ SepVeneto/free-dom@298659de9bc9441f8ac04c85fff7f6d7bb28df47 🚀

---
+++ assets/quick-start.md.510dc3a1.lean.js
@@ -0,0 +1,1 @@
+import{f as s,h as a,Q as e}from"./chunks/framework.450954f1.js";const l=e("",5),n=[l],i=JSON.parse('{"title":"快速开始","description":"","frontmatter":{"title":"快速开始"},"headers":[],"relativePath":"quick-start.md","filePath":"quick-start.md"}'),o={name:"quick-start.md"},d=Object.assign(o,{setup(t){return(p,c)=>(s(),a("div",null,n))}});export{i as __pageData,d as default};
